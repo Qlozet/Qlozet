107ef735f0ee449e933ffa585203291bcbd5a341
Your Name <promiseejiro43@gmail.com>
2024-04-11 08:39:44 +0100

    new commit

--- app/auth/signup/page.js
+++ app/auth/signup/page.js
@@ -13,7 +13,7 @@ const SignUp = () => {
   const router = useRouter();
   return (
     <section className="w-full bg-white p-4 flex items-center justify-center ">
-      <div className={`flex gap-8 max-w-7xl `}>
+      <div className={`${classes.container} flex gap-8 max-w-7xl `}>
         <div className={`${classes.first_Container} max-w-lg	`}>
           <Logo />
           <div className="mt-16">
