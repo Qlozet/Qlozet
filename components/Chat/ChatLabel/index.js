189872376ead2480f9bf3e6da40fe22fc666b3ce
Your Name <promiseejiro43@gmail.com>
2024-05-04 19:07:14 +0100

    new commit

--- components/Chat/ChatLabel/index.js
+++ components/Chat/ChatLabel/index.js
@@ -3,11 +3,11 @@ const ChatLabel = () => {
     <div className="flex items-center justify-center gap-10 ">
       <div className="flex items-center gap-3">
         <div className="w-[12px] h-[12px] rounded-[50%] bg-primary"></div>
-        <p>Male</p>
+        <p className=" bg-primary">Male</p>
       </div>
       <div className="flex items-center gap-3">
         <div className="w-[12px] h-[12px] rounded-[50%] bg-primary-100"></div>
-        <p>Female</p>
+        <p className=" bg-primary">Female</p>
       </div>
     </div>
   );
