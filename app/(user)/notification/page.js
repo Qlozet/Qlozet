baa359a6a30b59e4771d8cc75baf3524e2a3c3c6
EmosivbeThirdwishgroup <e.promise@thirdwishgroup.com>
2024-11-21 19:29:12 +0100

    new commmit

--- app/(user)/notification/page.js
+++ app/(user)/notification/page.js
@@ -9,10 +9,23 @@ import Notification from "@//components/Notification/NotificationComponent";
 import MobileSideBar from "@/components/MobileSideBar";
 import { getRequest } from "@/api/method";
 import Loader from "@/components/Loader";
+import Typography from "@/components/Typography";
 const NotificationPage = () => {
   const [showMobileNav, setShowMobileNav] = useState(false);
   const [pageLoading, setPageLoading] = useState(true);
-  const [notification, setNotification] = useState([]);
+  const [notification, setNotification] = useState([{
+    id: "Id",
+    read: false,
+    title: "Hello world Checking if the product was delivered.",
+    desc: "Hello world Checking if the product was delivered",
+    date: new Date().toISOString(),
+  }, {
+    id: "Id",
+    read: false,
+    title: "Hello world Checking if the product was delivered.",
+    desc: "Hello world Checking if the product was delivered",
+    date: new Date().toISOString(),
+  }]);
 
   const showSideBar = () => {
     setShowMobileNav(!showMobileNav);
@@ -34,7 +47,7 @@ const NotificationPage = () => {
           };
           notificationData.push(tableItem);
         });
-        setNotification(notificationData);
+        // setNotification(notificationData);
       } else {
         toast(<Toast text={response.message} type="danger" />);
       }
@@ -48,15 +61,25 @@ const NotificationPage = () => {
 
   return (
     <section>
-      <div className="flex bg-[#F8F9FA]">
-        <div className="w-full p-4">
+      <div className="flex mt-4">
+        <div className="w-full px-4 ">
           {pageLoading ? (
             <Loader></Loader>
           ) : (
-            <div className="border-[1px] border-solid border-gray-200 rounded-[12px] py-4 bg-white">
-              {notification.map((item, index) => (
+            <div className="rounded-[12px] overflow-hidden">
+              <div className="p-2 flex items-center justify-between mt-4 lg:mb-4 bg-[#F4F4F4] rounded-t-[12px] lg:hidden ">
+                <Typography
+                  textColor="text-[#121212]"
+                  textWeight="font-medium"
+                  textSize="text-[14px]"
+                >
+                  Notifications
+                </Typography>
+              </div>
+              <div className="bg-white">  {notification.map((item, index) => (
                 <Notification {...item} key={index} />
-              ))}
+              ))}</div>
+
             </div>
           )}
         </div>
