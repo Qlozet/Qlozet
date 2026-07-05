3805a87f21d4b97ee738f9732cbcf287796e0014
Kenzy567 <kennedyekechukwu3@gmail.com>
2026-07-05 01:09:12 -0400

    fix(ui): use standard Button component for add new user action

--- apps/vendor/src/components/Settings/UserAndPermission/UserAndPermssion/index.tsx
+++ apps/vendor/src/components/Settings/UserAndPermission/UserAndPermssion/index.tsx
@@ -1,10 +1,11 @@
 import { useState } from 'react';
 import Image from 'next/image';
-import Button from '@/components/Button';
-import addIcon from '@/public/assets/svg/add-square.svg';
+import { Plus } from 'lucide-react';
+import { Button } from '@/components/ui/button';
 import Modal from '@/components/Modal';
 import UserAndPermissionTable from '../UserAndPermissionTable';
 import AddNewUserAndPermissionForm from '../AddUserAndPermissionForm';
+
 const UserAndPermission = () => {
   const [showAddModal, setShowAddModal] = useState(false);
   const closeAddModal = () => {
@@ -18,20 +19,12 @@ const UserAndPermission = () => {
     <div className=''>
       <div className='flex items-center justify-end mb-4'>
         <Button
-          children={
-            <span className='flex justify-center items-center'>
-              <span>Add new user</span>
-              <Image src={addIcon} className='ml-4' />
-            </span>
-          }
-          btnSize='small'
-          minWidth='min-w-[14rem]'
-          variant='primary'
-          clickHandler={() => {
-            setShowAddModal(true);
-          }}
-        />
-        <div></div>
+          onClick={() => setShowAddModal(true)}
+          className='flex items-center space-x-2'
+        >
+          <Plus className='h-4 w-4' />
+          <span>Add new user</span>
+        </Button>
       </div>
       <UserAndPermissionTable handleEdit={handleEdit} />
 
